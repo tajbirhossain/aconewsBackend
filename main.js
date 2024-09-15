43d757e549264ce8fe94d7f640a3d0d1de20fb92
tajbirhossain <mdtajbirhossain031@gmail.com>
2024-09-15 12:54:36 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const GNEWS_API_URL = 'https://gnews.io/api/v4';
+const GNEWS_API_KEY = process.env.GNEWS_API_KEY;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/api/news', async (req, res) => {
+    try {
+        const { query, country, category, lang } = req.query;
+
+        
+        let url = `${GNEWS_API_URL}/top-headlines?token=${GNEWS_API_KEY}`;
+        if (query) url += `&q=${query}`;
+        if (country) url += `&country=${country}`;
+        if (category) url += `&topic=${category}`;
+        if (lang) url += `&lang=${lang}`;
+
+        const response = await axios.get(url);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch news' });
+    }
+});
+
+
+app.get('/api/news/:id', async (req, res) => {
+    try {
+        const newsId = req.params.id;
+        const response = await axios.get(`${GNEWS_API_URL}/article/${newsId}?token=${GNEWS_API_KEY}`);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch the news article' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
